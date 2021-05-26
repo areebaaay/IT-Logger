48894b3e528149774dce350266663f9dc8a9e984
Areeba Akhtar <areeba.akhtar776@gmail.com>
2021-05-26 09:28:54 +0500

    initial commit

---
+++ src/components/logs/Logs.js
@@ -0,0 +1,41 @@
+import { useEffect } from 'react';
+import { connect } from 'react-redux';
+import LogItem from './LogItem';
+import Preloader from '../layout/Preloader';
+import PropTypes from 'prop-types';
+import { getLogs } from '../../action/logActions';
+
+const Logs = ({ log: { logs, loading }, getLogs }) => {
+  useEffect(() => {
+    getLogs();
+  }, []);
+
+  if (loading || logs === null) {
+    return <Preloader />;
+  }
+
+  return (
+    <ul className="collection with-header">
+      <li className="collection-header">
+        <h4>System Logs</h4>
+      </li>
+      {!loading && logs.length === 0 ? (
+        <p className="center">No logs to show...</p>
+      ) : (
+        logs.map((log) => <LogItem log={log} key={log.id} />)
+      )}
+    </ul>
+  );
+};
+
+Logs.propTypes = {
+  log: PropTypes.object.isRequired,
+  getLogs: PropTypes.func.isRequired,
+};
+
+const mapStateToProp = (state) => ({
+  //state.log is reffering to the log created in main reducer => reducer/index.js
+  log: state.log,
+});
+
+export default connect(mapStateToProp, { getLogs })(Logs);
