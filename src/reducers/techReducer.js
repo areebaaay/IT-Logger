6145a6607ee7b160ecbc04e8c095e506a90b9875
Areeba Akhtar <areeba.akhtar776@gmail.com>
2021-05-26 12:17:56 +0500

    delete technician

--- src/reducers/techReducer.js
+++ src/reducers/techReducer.js
@@ -1,4 +1,10 @@
-import { GET_TECHS, SET_LOADING } from '../action/types';
+import {
+  ADD_TECH,
+  DELETE_TECH,
+  GET_TECHS,
+  SET_LOADING,
+  TECHS_ERROR,
+} from '../action/types';
 
 const initialState = {
   techs: null,
@@ -20,6 +26,30 @@ const techReducer = (state = initialState, action) => {
       return {
         ...state,
         loading: true,
+      };
+    }
+
+    case ADD_TECH: {
+      return {
+        ...state,
+        techs: [...state.techs, action.payload],
+        loading: false,
+      };
+    }
+
+    case DELETE_TECH:
+      return {
+        ...state,
+        techs: state.techs.filter((tech) => tech.id !== action.payload),
+        loading: false,
+      };
+
+    case TECHS_ERROR: {
+      console.error(action.payload);
+      return {
+        ...state,
+        error: action.payload,
+        loading: false,
       };
     }
 
